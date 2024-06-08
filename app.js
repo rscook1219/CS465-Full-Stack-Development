1f6d4a5e11adcd3ea95c15f76c5fd6e71d74adbf
Robert Cook <rsc1219@yahoo.com>
2024-06-08 15:41:57 -0600

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travleRouter = require('./app_server/routes/travel');
+var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+//Bring in the databse
+require('./app_api/models/db');
+
 var app = express();
 
 // view engine setup
@@ -24,9 +31,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/travel', travleRouter);
+app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
