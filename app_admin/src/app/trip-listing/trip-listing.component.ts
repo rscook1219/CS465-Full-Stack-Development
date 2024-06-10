7e8d192a48c4e28ffca20fb49c42a3fde9d502fd
Robert Cook <rsc1219@yahoo.com>
2024-06-10 17:07:20 -0600

    Started Implementing Angular

---
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trips } from '../data/trips';
+
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-listing.component.html',
+  styleUrl: './trip-listing.component.css'
+})
+export class TripListingComponent {
+  trips: Array<any> = trips;
+
+  constructor() {}
+
+  ngOnInit(): void {
+        
+  }
+
+}
