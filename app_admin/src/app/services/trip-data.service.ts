54972225d801948953040c5e045037734268be1d
Robert Cook <rsc1219@yahoo.com>
2024-06-16 14:03:32 -0600

    Module 6 completed baseline

---
+++ app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import {Trip } from '../models/trip';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripDataService {
+
+  constructor(private http: HttpClient) {}
+
+  getTrips(): Observable<Trip[]> {
+    let url = 'http://localhost:3000/api/travel';
+
+    return this.http.get<Trip[]>(url);
+  }
+
+}
